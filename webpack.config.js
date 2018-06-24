0b66a9c8a532dc393379b9338ed97060a98a4a76
usbpendrive <ferry.fernando@gmail.com>
2018-06-24 09:34:56 +0700

    Create basic Vue application with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
 
 const config = {
     entry: {
@@ -11,12 +12,41 @@ const config = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, '/dist'),
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                },
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            vue$: 'vue/dist/vue.esm.js',
+        },
+    },
     plugins: [
         new HtmlWebpackPlugin({ template: 'src/index.html' }),
         new CleanWebpackPlugin(['dist']),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
         }),
+        new VueLoaderPlugin(),
     ],
 };
 
